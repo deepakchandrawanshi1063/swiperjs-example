14d2ff3c4057614ca6d331a0b4a4132bac2b3db3
Deepak Kumar <deepakchandrawanshi1063@gmail.com>
2024-01-18 20:19:49 +0530

    swiperjs example tested

--- src/App.js
+++ src/App.js
@@ -1,25 +1,94 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef, useState } from "react";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
 
-function App() {
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+
+import "./App.css";
+
+// import required modules
+import { Pagination } from "swiper/modules";
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Swiper
+        slidesPerView={1}
+        centeredSlides={true}
+        spaceBetween={30}
+        pagination={{
+          clickable: true,
+        }}
+        modules={[Pagination]}
+        className="mySwiper"
+        // direction={"vertical"}
+      >
+        <SwiperSlide>
+          <iframe
+            width="800"
+            height="350"
+            src="https://www.youtube.com/embed/8eWgYd2G-ew?si=MNMruyD-XWfIax-h"
+            title="YouTube video player"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            allowfullscreen
+            style={{
+              borderRadius: "50px",
+              // boxShadow: "rgba(0, 0, 0, 0.35) 0px 5px 15px",
+            }}
+          ></iframe>
+        </SwiperSlide>
+        <SwiperSlide>
+          <iframe
+            width="800"
+            height="350"
+            src="https://www.youtube.com/embed/8eWgYd2G-ew?si=MNMruyD-XWfIax-h"
+            title="YouTube video player"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            allowfullscreen
+            style={{ borderRadius: "50px" }}
+          ></iframe>
+        </SwiperSlide>
+        <SwiperSlide>
+          <iframe
+            width="800"
+            height="350"
+            src="https://www.youtube.com/embed/8eWgYd2G-ew?si=MNMruyD-XWfIax-h"
+            title="YouTube video player"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            allowfullscreen
+            style={{ borderRadius: "50px" }}
+          ></iframe>
+        </SwiperSlide>
+        <SwiperSlide>
+          <iframe
+            width="800"
+            height="350"
+            src="https://www.youtube.com/embed/8eWgYd2G-ew?si=MNMruyD-XWfIax-h"
+            title="YouTube video player"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            allowfullscreen
+            style={{ borderRadius: "50px" }}
+          ></iframe>
+        </SwiperSlide>
+        <SwiperSlide>
+          <iframe
+            width="800"
+            height="350"
+            src="https://www.youtube.com/embed/8eWgYd2G-ew?si=MNMruyD-XWfIax-h"
+            title="YouTube video player"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            style={{ borderRadius: "50px" }}
+            allowfullscreen
+          ></iframe>
+        </SwiperSlide>
+      </Swiper>
+    </>
   );
 }
-
-export default App;
